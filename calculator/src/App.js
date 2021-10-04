79d0b8956747ae2b83ff37ce9e8f879bbec02b10
scorteaalexandra <68050250+scorteaalexandra@users.noreply.github.com>
2021-10-04 22:22:17 +0300

    #1 +Add few buttons

---
+++ calculator/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+	return (
+		<div className="calculator">
+			<div className="display">
+				<span>(0)</span>0
+			</div>
+			<button>/</button>
+			<button>*</button>
+			<button>+</button>
+			<button>-</button>
+			<button>DEL</button>
+
+		</div>
+	);
+}
+
+export default App;
