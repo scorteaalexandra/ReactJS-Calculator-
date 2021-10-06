2ae88208d6e6c0e6a8dc7f7fd6d3e964ca02745e
scorteaalexandra <68050250+scorteaalexandra@users.noreply.github.com>
2021-10-06 09:23:50 +0300

    #2

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -20,11 +20,13 @@ return digits;
 			<div className="display">
 				<span>(0)</span>0
 			</div>
+			<div className="operators">
 			<button>/</button>
 			<button>*</button>
 			<button>+</button>
 			<button>-</button>
 			<button>DEL</button>
+			</div>
 
 			<div className="digits">
 				{createDigits()}
