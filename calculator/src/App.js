e1ca608fa4902b15220c88f187b24a3ad3bb31be
scorteaalexandra <68050250+scorteaalexandra@users.noreply.github.com>
2021-10-12 11:41:50 +0300

    #3 functionalitate butoane +-*/

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,7 +1,15 @@
 
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+const [calc,setCalc]=useState("");
+const [result,setResult]=useState(" ");
+const ops=['/','*','+','-',];
+
+const updateCalc=value=>{
+	setCalc(calc+value);
+}
 
 	const createDigits=()=>{
 const digits=[];
@@ -18,13 +26,13 @@ return digits;
 	return (
 		<div className="calculator">
 			<div className="display">
-				<span>(0)</span>0
+				{result ? <span>(0)</span>: ' '}{calc||"0"}
 			</div>
 			<div className="operators">
-			<button>/</button>
-			<button>*</button>
-			<button>+</button>
-			<button>-</button>
+			<button onClick={()=>updateCalc('/')}>/</button>
+			<button onClick={()=>updateCalc('*')}>*</button>
+			<button onClick={()=>updateCalc('+')}>+</button>
+			<button onClick={()=>updateCalc('-')}>-</button>
 			<button>DEL</button>
 			</div>
 
